ab4ed8264a85f4ed7f432213c50e5236056f380d
sh.oha.me <128396413+shohame1003@users.noreply.github.com>
2024-07-26 18:32:17 +0500

    stringToIntegerAtoi

---
+++ src/stringToIntegerAtoi/Solution.java
@@ -0,0 +1,41 @@
+package stringToIntegerAtoi;
+
+public class Solution {
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        int i = 0;
+        boolean negetiveSign = false;
+        char[] input = str.toCharArray();
+        while (i < input.length && input[i] == ' ') {
+            i++;
+        }
+        if (i == input.length) {
+            return 0;
+        } else if (input[i] == '+') {
+            i++;
+        } else if (input[i] == '-') {
+            i++;
+            negetiveSign = true;
+        }
+        int num = 0;
+        while (i < input.length && input[i] <= '9' && input[i] >= '0') {
+            // current char
+            int tem = input[i] - '0';
+            tem = negetiveSign ? -tem : tem;
+            // avoid invalid number like 038
+            if (num == 0 && tem == '0') {
+                i++;
+            } else if (num == Integer.MIN_VALUE / 10 && tem <= -8 || num < Integer.MIN_VALUE / 10) {
+                return Integer.MIN_VALUE;
+            } else if (num == Integer.MAX_VALUE / 10 && tem >= 7 || num > Integer.MAX_VALUE / 10) {
+                return Integer.MAX_VALUE;
+            } else {
+                num = num * 10 + tem;
+                i++;
+            }
+        }
+        return num;
+    }
+}
