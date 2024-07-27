ea04d5f122e125c823abf025ad194f0ed5bcf66b
sh.oha.me <128396413+shohame1003@users.noreply.github.com>
2024-07-27 21:56:03 +0500

    palindromeNumber

---
+++ src/palindromeNumber/Solution.java
@@ -0,0 +1,17 @@
+package palindromeNumber;
+
+public  class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int rev = 0;
+        int localX = x;
+        while (localX > 0) {
+            rev *= 10;
+            rev += localX % 10;
+            localX /= 10;
+        }
+        return rev == x;
+    }
+}
