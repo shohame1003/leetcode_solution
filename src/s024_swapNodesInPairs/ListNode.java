9d14c16457c64fff5bc762bdfd4b6e579b9c9652
sh.oha.me <128396413+shohame1003@users.noreply.github.com>
2024-07-30 20:59:57 +0500

    s024_swapNodesInPairs

---
+++ src/s024_swapNodesInPairs/ListNode.java
@@ -0,0 +1,9 @@
+package s024_swapNodesInPairs;
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
